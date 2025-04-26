acb16e06e96512a369a554d96cf357d77f30fffa
jeeva-004 <mjeevanantham004@gmail.com>
2025-04-26 13:52:51 +0530

    modified

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-var nav_menu = document.querySelector('nav')
-
-let hamburger = document.querySelector('.hamburger').addEventListener('click',()=>{
-    
+var nav_menu = document.querySelector('nav');
+document.querySelector('.hamburger').addEventListener('click',()=>{
+    nav_menu.style.display ='flex';
+})
+document.querySelector('.close').addEventListener('click',()=>{
+    nav_menu.style.display ='none';
+    // nav_menu.style.animation =  'closeMenu .25s ease 0s 1 backwards';
 })
