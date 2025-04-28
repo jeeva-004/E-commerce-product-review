94dd683db604ce542dd7dfa607944d3075b7d140
jeeva-004 <mjeevanantham004@gmail.com>
2025-04-28 18:38:57 +0530

    content modified

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+// navbar 
 var nav_menu = document.querySelector('nav');
-document.querySelector('.hamburger').addEventListener('click',()=>{
-    nav_menu.style.display ='flex';
-})
-document.querySelector('.close').addEventListener('click',()=>{
-    nav_menu.style.display ='none';
-})
+document.querySelector('.hamburger').addEventListener('click', () => nav_menu.style.display = 'flex')
+document.querySelector('.close').addEventListener('click', () => nav_menu.style.display = 'none')
+
+let plus = document.querySelector('.plus');
+let minus = document.querySelector('.minus');
+let item_count = document.querySelector('.item-count');
+
+plus.addEventListener('click', () => item_count.innerHTML++)
+minus.addEventListener('click', () => item_count.innerHTML--)
