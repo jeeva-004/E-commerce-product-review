83edf52cabff986eb5cf5668706b5996783d5c9d
jeeva-004 <mjeevanantham004@gmail.com>
2025-04-29 16:53:46 +0530

    image content modified

--- script.js
+++ script.js
@@ -36,27 +36,63 @@ document.querySelector('.button').addEventListener('click', () => {
 
 let img_list = ['image-product-1', 'image-product-2', 'image-product-3', 'image-product-4'];
 let index = 0;
-let arrows = document.querySelectorAll('.arrow')
-let preview_image = document.querySelector('.preview')
+let arrows = document.querySelectorAll('.arrow');
+let preview_image = document.querySelector('.preview');
 arrows.forEach(button => {
     button.addEventListener('click', () => {
         if (button.classList.contains('forward')) {
             index++;
-            if (index > img_list.length-1) {
+            if (index > img_list.length - 1) {
                 index = 0;
             }
-            preview_image.src = `./images/${img_list[index]}.jpg`
+            preview_image.src = `./images/${img_list[index]}.jpg`;
         }
         else {
             index--;
             if (index < 0) {
-                index = img_list.length-1;
+                index = img_list.length - 1;
             }
-            preview_image.src = `./images/${img_list[index]}.jpg`
+            preview_image.src = `./images/${img_list[index]}.jpg`;
 
         }
 
     })
+})
+
+//display images
+
+let img_1 = document.querySelector('.preview-img1');
+let img_2 = document.querySelector('.preview-img2');
+let img_3 = document.querySelector('.preview-img3');
+let img_4 = document.querySelector('.preview-img4');
+
+img_1.addEventListener('click', () => {
+    preview_image.src = img_1.src;
+    img_3.classList.remove('active')
+    img_2.classList.remove('active')
+    img_4.classList.remove('active')
+    img_1.classList.add('active')
+})
+img_2.addEventListener('click', () => {
+    img_3.classList.remove('active')
+    img_1.classList.remove('active')
+    img_4.classList.remove('active')
+    img_2.classList.add('active')
+    preview_image.src = img_2.src;
+})
+img_3.addEventListener('click', () => {
+    preview_image.src = img_3.src;
+    img_2.classList.remove('active')
+    img_1.classList.remove('active')
+    img_4.classList.remove('active')
+    img_3.classList.add('active')
+})
+img_4.addEventListener('click', () => {
+    preview_image.src = img_4.src;
+    img_3.classList.remove('active')
+    img_1.classList.remove('active')
+    img_2.classList.remove('active')
+    img_4.classList.add('active')
 })
 
 
